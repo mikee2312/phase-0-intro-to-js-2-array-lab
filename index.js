e2da5f4ca9dd4e60555281e4b980c8ce02d9f098
mikee2312 <124088547+mikee2312@users.noreply.github.com>
2023-02-03 23:48:32 -0500

    completed all task

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+   const appCats = [...cats, name]
+    return appCats;
+}
+
+function prependCat(name) {
+    const prepCats = [name, ...cats]
+    return prepCats
+}
+
+function removeLastCat() {
+    const remLastCats = cats.slice(0, 2)
+    return remLastCats
+}
+function removeFirstCat() {
+    const remFirstCats = cats.slice(1)
+    return remFirstCats
+}
